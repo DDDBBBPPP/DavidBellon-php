3fa28e0f7b9ff055b66a22e6298e9bce16133fb5
david <tuemail@dominio.com>
2025-12-29 04:23:36 +0100

    Fix: autoload case-sensitive for Railway (Linux)

--- src/autoload.php
+++ src/autoload.php
@@ -1,10 +1,24 @@
 <?php
 
+
+$composerAutoload = __DIR__ . "/vendor/autoload.php";
+if (file_exists($composerAutoload)) {
+    require_once $composerAutoload;
+}
+
 spl_autoload_register(function ($class) {
+
     $ruta = str_replace("\\", "/", $class) . ".php";
 
-    if (file_exists($ruta)) {
-        require_once $ruta;
+
+    $ruta = preg_replace_callback('/^([^\/]+)\//', function ($m) {
+        return strtolower($m[1]) . "/";
+    }, $ruta);
+
+    $fullPath = __DIR__ . "/" . $ruta;
+
+    if (file_exists($fullPath)) {
+        require_once $fullPath;
     }
 });
 
